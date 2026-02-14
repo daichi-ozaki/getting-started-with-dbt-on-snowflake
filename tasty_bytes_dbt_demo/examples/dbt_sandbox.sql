9f45622f7925766cc873060e91443cb873476f24
daichi-ozaki <daichi.ozaki@paypay-corp.co.jp>
2026-02-14 06:40:16 +0000

    TASK作成追加

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -3,3 +3,14 @@ SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
 SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
 
 SHOW DBT PROJECTS LIKE 'tasty%';
+
+select * from tasty_bytes_dbt_db.raw.order_header
+limit 10;
+
+USE ROLE ACCOUNTADMIN;
+
+CREATE OR REPLACE TASK tasty_bytes_dbt_db.dev.run_prepped_data_dbt
+        WAREHOUSE=tasty_bytes_dbt_wh
+        SCHEDULE ='USING CRON 1 * * * * America/Los_Angeles'
+      AS
+  EXECUTE DBT PROJECT tasty_bytes_dbt_project ARGS='run --select customer_loyalty_metrics --target dev';
